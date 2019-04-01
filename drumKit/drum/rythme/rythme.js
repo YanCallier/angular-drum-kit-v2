673065aa4f0cc09e5b7bfc86fca085e40ecd31b5
Callier <45624004+YanCallier@users.noreply.github.com>
2019-04-01 18:18:56 +0200

    Add files via upload

--- drumKit/drum/rythme/rythme.js
+++ drumKit/drum/rythme/rythme.js
@@ -2,33 +2,12 @@ angular.
 module('drumApp')
 .component('rythme', {
         templateUrl: 'drumKit/drum/rythme/rythme.html',
-        // bindings:{
-        //     sounds: '<'
-        //   },
         controller: function rythmeController($location, $scope) {
             var vm = this;
             this.queries = $location.search();
             $scope.$on('$locationChangeStart', function() { 
                 vm.queries = $location.search();
               });
-            // this.$onChanges = function (){
-            //     this.sound = $location.hash().split('');
-            //     this.soundsTable =  $location.hash().split('')
-                this.player = function (){
-                    //var count=0;
-                    // for (let sound of this.soundsTable ) {
-                    //     sound = sound.trim();
-    
-                    //     setTimeout(function (){
-                    //         if (sound != ""){
-                    //             var audio = new Audio('drumKit/sounds/' + sound + '.wav');
-                    //             audio.play();
-                    //         }
-                    //     },count*200);
-                    //     count +=1;
-                    // }   
-                }
-            //}
         }
     },
 );
